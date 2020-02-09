1117909af0dd116a6201d675d352a2007343c36b
project-coder <project.coder.git@gmail.com>
2020-02-09 15:13:30 -0300

    primeira versao

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Main from './pages/main';
+
+const App = () => <Main />;
+
+export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+
+import {View, Text, TextInput, Button, Alert, StyleSheet} from 'react-native';
+import Voice from 'react-native-voice';
+import Tts from 'react-native-tts';
+
+const numbers = [1, 2, 3, 4, 5];
+
+    
+export default class Main extends Component {   
+    constructor(props) {
+
+        super(props)
+        this.state = {texto:''} 
+        Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
+        
+    
+      }
+    
+       onSpeechPartialResults(result){
+            this.setState({
+                ...this.state,
+                texto:result.value
+            });
+       }    
+       
+
+     
+  render() {
+    return (
+      <View style={estilo.tela} onTouchEnd={() => Voice.start('pt-BR')}>
+        <TextInput editable={false} style={estilo.txt} onSpeechPartial>{this.state.texto}</TextInput>
+         <TextInput><li>{6}</li></TextInput>
+        
+      </View>
+    );
+  }
+}
+
+
+
+const estilo = StyleSheet.create({
+    tela: {
+        width: '100%', 
+        height: '100%',
+        backgroundColor: 'powderblue',
+        justifyContent: 'center',
+        alignItems: 'center'
+        },
+
+    txt: {
+        fontWeight: 'bold',
+    },
+    
+
+});
+
